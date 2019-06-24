4a1de61aa7b27be58f5ba6a9465b8cc0df71a016
Tony <tonysobreira@gmail.com>
2019-06-24 13:53:10 -0300

    Update Client.java

--- src/main/java/com/clientrestservice/model/Client.java
+++ src/main/java/com/clientrestservice/model/Client.java
@@ -18,38 +18,38 @@ public class Client {
 	@Column(name = "id")
 	private Integer id;
 
-	@Column(name = "nome")
-	private String nome;
+	@Column(name = "name")
+	private String name;
 
 	@Column(name = "cpf")
 	private String cpf;
 
-	@Column(name = "dataNascimento")
-	private Date dataNascimento;
+	@Column(name = "birthdate")
+	private Date birthDate;
 
-	@Column(name = "telefone")
-	private String telefone;
+	@Column(name = "phone")
+	private String phone;
 
 	@Column(name = "email")
 	private String email;
 
-	@Column(name = "logradouro")
-	private String logradouro;
+	@Column(name = "address")
+	private String address;
 
-	@Column(name = "cep")
-	private String cep;
+	@Column(name = "postalcode")
+	private String postalCode;
 
-	@Column(name = "bairro")
-	private String bairro;
+	@Column(name = "neighborhood")
+	private String neighborhood;
 
-	@Column(name = "complemento")
-	private String complemento;
+	@Column(name = "complement")
+	private String complement;
 
-	@Column(name = "cidade")
-	private String cidade;
+	@Column(name = "city")
+	private String city;
 
-	@Column(name = "estado")
-	private String estado;
+	@Column(name = "state")
+	private String state;
 
 	public Integer getId() {
 		return id;
@@ -59,12 +59,12 @@ public class Client {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getCpf() {
@@ -75,20 +75,20 @@ public class Client {
 		this.cpf = cpf;
 	}
 
-	public Date getDataNascimento() {
-		return dataNascimento;
+	public Date getBirthDate() {
+		return birthDate;
 	}
 
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
 	}
 
-	public String getTelefone() {
-		return telefone;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public String getEmail() {
@@ -99,70 +99,70 @@ public class Client {
 		this.email = email;
 	}
 
-	public String getLogradouro() {
-		return logradouro;
+	public String getAddress() {
+		return address;
 	}
 
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
-	public String getCep() {
-		return cep;
+	public String getPostalCode() {
+		return postalCode;
 	}
 
-	public void setCep(String cep) {
-		this.cep = cep;
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 
-	public String getBairro() {
-		return bairro;
+	public String getNeighborhood() {
+		return neighborhood;
 	}
 
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
 	}
 
-	public String getComplemento() {
-		return complemento;
+	public String getComplement() {
+		return complement;
 	}
 
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
+	public void setComplement(String complement) {
+		this.complement = complement;
 	}
 
-	public String getCidade() {
-		return cidade;
+	public String getCity() {
+		return city;
 	}
 
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
+	public void setCity(String city) {
+		this.city = city;
 	}
 
-	public String getEstado() {
-		return estado;
+	public String getState() {
+		return state;
 	}
 
-	public void setEstado(String estado) {
-		this.estado = estado;
+	public void setState(String state) {
+		this.state = state;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
-		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
-		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
-		result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((complement == null) ? 0 : complement.hashCode());
 		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((logradouro == null) ? 0 : logradouro.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((neighborhood == null) ? 0 : neighborhood.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((postalCode == null) ? 0 : postalCode.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
 		return result;
 	}
 
@@ -175,75 +175,74 @@ public class Client {
 		if (getClass() != obj.getClass())
 			return false;
 		Client other = (Client) obj;
-		if (bairro == null) {
-			if (other.bairro != null)
+		if (address == null) {
+			if (other.address != null)
 				return false;
-		} else if (!bairro.equals(other.bairro))
+		} else if (!address.equals(other.address))
 			return false;
-		if (cep == null) {
-			if (other.cep != null)
+		if (birthDate == null) {
+			if (other.birthDate != null)
 				return false;
-		} else if (!cep.equals(other.cep))
+		} else if (!birthDate.equals(other.birthDate))
 			return false;
-		if (cidade == null) {
-			if (other.cidade != null)
+		if (city == null) {
+			if (other.city != null)
 				return false;
-		} else if (!cidade.equals(other.cidade))
+		} else if (!city.equals(other.city))
 			return false;
-		if (complemento == null) {
-			if (other.complemento != null)
+		if (complement == null) {
+			if (other.complement != null)
 				return false;
-		} else if (!complemento.equals(other.complemento))
+		} else if (!complement.equals(other.complement))
 			return false;
 		if (cpf == null) {
 			if (other.cpf != null)
 				return false;
 		} else if (!cpf.equals(other.cpf))
 			return false;
-		if (dataNascimento == null) {
-			if (other.dataNascimento != null)
-				return false;
-		} else if (!dataNascimento.equals(other.dataNascimento))
-			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
 		} else if (!email.equals(other.email))
-			return false;
-		if (estado == null) {
-			if (other.estado != null)
-				return false;
-		} else if (!estado.equals(other.estado))
 			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (logradouro == null) {
-			if (other.logradouro != null)
+		if (name == null) {
+			if (other.name != null)
 				return false;
-		} else if (!logradouro.equals(other.logradouro))
+		} else if (!name.equals(other.name))
 			return false;
-		if (nome == null) {
-			if (other.nome != null)
+		if (neighborhood == null) {
+			if (other.neighborhood != null)
 				return false;
-		} else if (!nome.equals(other.nome))
+		} else if (!neighborhood.equals(other.neighborhood))
 			return false;
-		if (telefone == null) {
-			if (other.telefone != null)
+		if (phone == null) {
+			if (other.phone != null)
 				return false;
-		} else if (!telefone.equals(other.telefone))
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (postalCode == null) {
+			if (other.postalCode != null)
+				return false;
+		} else if (!postalCode.equals(other.postalCode))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Client [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", dataNascimento=" + dataNascimento
-				+ ", telefone=" + telefone + ", email=" + email + ", logradouro=" + logradouro + ", cep=" + cep
-				+ ", bairro=" + bairro + ", complemento=" + complemento + ", cidade=" + cidade + ", estado=" + estado
-				+ "]";
+		return "Client [id=" + id + ", name=" + name + ", cpf=" + cpf + ", birthDate=" + birthDate + ", phone=" + phone
+				+ ", email=" + email + ", address=" + address + ", postalCode=" + postalCode + ", neighborhood="
+				+ neighborhood + ", complement=" + complement + ", city=" + city + ", state=" + state + "]";
 	}
 
 }
